6790f96df3c33c42a5fb5d83a552682f581f71fb
Diego Santos <diegofmsantos0@gmail.com>
2023-02-27 16:42:01 -0300

    projeto finalizado

---
+++ script.js
@@ -0,0 +1,17 @@
+// VARIÁVEIS
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade")
+
+// FUNÇÕES
+const toggleModal = () => {
+    [modal, fade].forEach((el) => el.classList.toggle('hide'))
+    
+}
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+    el.addEventListener("click", () => toggleModal());
+  });
+
+// EVENTOS
